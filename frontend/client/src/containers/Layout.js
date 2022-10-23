8ee6ee4efb1c330d13c6c2f976a493bff209553a
cvijetin-mladenovic <cvijetin.mladenovic@hpe.com>
2022-10-23 12:02:06 +0200

    Added login and register pages

--- frontend/client/src/containers/Layout.js
+++ frontend/client/src/containers/Layout.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { Layout, Menu, Breadcrumb } from "antd";
-import { HomeOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  LoginOutlined,
+  PlusCircleTwoTone,
+} from "@ant-design/icons";
 
 const { Header, Content, Footer, Sider } = Layout;
+
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Home from "../pages/Home";
 
 function CustomLayout() {
   const [collapsed, setCollapsed] = useState(false);
@@ -23,6 +31,12 @@ function CustomLayout() {
           <Menu.Item key="1" icon={<HomeOutlined />}>
             <Link to="/">Home</Link>
           </Menu.Item>
+          <Menu.Item key="2" icon={<PlusCircleTwoTone />}>
+            <Link to="/register">Register</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<LoginOutlined />}>
+            <Link to="/login">Login</Link>
+          </Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
@@ -37,12 +51,14 @@ function CustomLayout() {
             style={{ padding: 24, minHeight: 360 }}
           >
             <Routes>
-              <Route exact path="/"></Route>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
             </Routes>
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
-          Generic footer placeholder
+          Savremene obrazovne tehnologije i standardi ©
         </Footer>
       </Layout>
     </Layout>
