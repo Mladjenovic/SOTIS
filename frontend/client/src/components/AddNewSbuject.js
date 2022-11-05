7c1023de1ee00f28db8d2409f0bb0de06f63dd57
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2022-11-05 11:21:41 +0100

    admin_page (#3)
    * Update create subject page
    
    * update all
    
    Co-authored-by: cvijetin-mladenovic <cvijetin.mladenovic@hpe.com>

--- frontend/client/src/components/AddNewSbuject.js
+++ frontend/client/src/components/AddNewSbuject.js
@@ -1,95 +1,171 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Form, Input, Select } from "antd";
 const { Option } = Select;
+import axios from "axios";
+import loader from "../assets/loader.gif";
+import { toastOptions } from "../utils/constants";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { getAllProfesorsRoute, createSubjectRoute } from "../utils/APIRoutes";
 
 function AddNewSbuject() {
+  const [data, setData] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [professors, setProfessors] = useState([]);
+
   const onFinish = (values) => {
-    console.log("Success:", values);
+    const token = JSON.stringify(localStorage.getItem("access-token"));
+    const url = createSubjectRoute + `/${values.profesorForSubject}`;
+
+    axios
+      .post(
+        url,
+        {
+          title: values.title,
+          description: values.description,
+          minimumPoints: values.minimumPoints,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(
+              JSON.stringify(localStorage.getItem("access-token"))
+            )}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log("aaaaaaaaaa", res);
+        toast.success("successfully created subject", toastOptions);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
+    toast.error(error.message, toastOptions);
   };
 
+  useEffect(() => {
+    setCurrentUser(JSON.parse(localStorage.getItem("sotis-app-user")));
+    axios
+      .get(getAllProfesorsRoute, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(
+            JSON.stringify(localStorage.getItem("access-token"))
+          )}`,
+        },
+      })
+      .then((res) => {
+        res.data.forEach((el) => {
+          professors.push({
+            id: el.id,
+            name: el.name,
+            role: el.role,
+            surname: el.surname,
+            username: el.username,
+          });
+        });
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>
-      <Form
-        style={{ marginTop: "4rem" }}
-        name="basic"
-        labelCol={{
-          span: 5,
-        }}
-        wrapperCol={{
-          span: 12,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Title"
-          name="title"
-          rules={[
-            {
-              required: true,
-              message: "Please input subject title!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Description"
-          name="description"
-          rules={[
-            {
-              required: true,
-              message: "Please input subject Description!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="MinimumPoints"
-          name="minimumPoints"
-          rules={[
-            {
-              required: true,
-              message: "Please input subject MinimumPoints!",
-            },
-          ]}
-        >
-          <Input type="number" />
-        </Form.Item>
+    <>
+      {isLoading ? (
+        <img src={loader} alt="loader" className="loader" />
+      ) : (
+        <div>
+          <Form
+            style={{ marginTop: "4rem" }}
+            name="basic"
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 12,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input subject title!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Description"
+              name="description"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input subject Description!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="MinimumPoints"
+              name="minimumPoints"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input subject MinimumPoints!",
+                },
+              ]}
+            >
+              <Input type="number" />
+            </Form.Item>
 
-        <Form.Item
-          name="profesorForSubject"
-          rules={[{ required: true }]}
-          label="Select Profesor"
-          wrapperCol={{
-            offset: 0,
-            span: 12,
-          }}
-        >
-          <Select placeholder="Select a option and change input text above">
-            <Option value="male">male</Option>
-            <Option value="female">female</Option>
-            <Option value="other">other</Option>
-          </Select>
-        </Form.Item>
+            <Form.Item
+              name="profesorForSubject"
+              rules={[{ required: true }]}
+              label="Select Profesor"
+              wrapperCol={{
+                offset: 0,
+                span: 12,
+              }}
+            >
+              <Select placeholder="Select a option and change input text above">
+                {professors.map((option) => (
+                  <Option key={option.id} value={option.id}>
+                    {option.name}
+                  </Option>
+                ))}
+                {/* <Option value="male">male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">other</Option> */}
+              </Select>
+            </Form.Item>
 
-        <Form.Item
-          wrapperCol={{
-            offset: 5,
-            span: 16,
-          }}
-        >
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+            <Form.Item
+              wrapperCol={{
+                offset: 5,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      )}
+    </>
   );
 }
 
--- frontend/client/src/pages/AllSubjectsAdmin.js
+++ frontend/client/src/pages/AllSubjectsAdmin.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Space, Table, Tag } from "antd";
+import { getAllSubjectsRoute } from "../utils/APIRoutes";
+import loader from "../assets/loader.gif";
 
 function AllSubjectsAdmin() {
-  return <div style={{ color: "black" }}>AllSubjectsAdmin</div>;
+  const [subjects, setSubjects] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "MinimumPoints",
+      dataIndex: "minimumPoints",
+      key: "minimumPoints",
+    },
+  ];
+
+  useEffect(() => {
+    setCurrentUser(JSON.parse(localStorage.getItem("sotis-app-user")));
+    axios
+      .get(getAllSubjectsRoute, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(
+            JSON.stringify(localStorage.getItem("access-token"))
+          )}`,
+        },
+      })
+      .then((res) => {
+        setSubjects(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <img src={loader} alt="loader" className="loader" />
+      ) : (
+        <div style={{ color: "black" }}>
+          <Table dataSource={subjects} columns={columns} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default AllSubjectsAdmin;
