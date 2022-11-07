81d768ba2faf085939cfb89498cf9af0cee8c61e
Korma96 <markoradovic996@gmail.com>
2022-11-07 08:01:36 +0100

    Fix git ignore

---
+++ SOTIS-backend/SOTIS-backend/Controllers/Dtos/TestResult/TestResultRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SOTIS_backend.Controllers.Dtos
+{
+    public class TestResultRequestDto
+    {
+        public string TestId { get; set; }
+
+        public IEnumerable<IdDto> SelectedStudentAnswers { get; set; }
+    }
+}
---
+++ SOTIS-backend/SOTIS-backend/Controllers/Dtos/TestResult/TestResultResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SOTIS_backend.Controllers.Dtos
+{
+    public class TestResultResponseDto
+    {
+        public string StudentUsername { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string StudentSurname { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public double Points { get; set; }
+    }
+}
