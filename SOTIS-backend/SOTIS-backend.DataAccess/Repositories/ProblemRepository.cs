d98f164591b1a8344b89aeb1ae7574708313177d
Korma96 <markoradovic996@gmail.com>
2022-11-11 15:19:30 +0100

    Problems CRUD

---
+++ SOTIS-backend/SOTIS-backend.DataAccess/Repositories/ProblemRepository.cs
@@ -0,0 +1,10 @@
+﻿using SOTIS_backend.DataAccess.Interfaces;
+using SOTIS_backend.DataAccess.Models;
+
+namespace SOTIS_backend.DataAccess.Repositories
+{
+    public class ProblemRepository : EntityBaseRepository<Problem>, IProblemRepository
+    {
+        public ProblemRepository(SotisDbContext context) : base(context) { }
+    }
+}
